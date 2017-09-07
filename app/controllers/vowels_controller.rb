df5486d34bdb149c601d9ef30d826249e49ba98c
Joe Seiden <joe.seiden214@gmail.com>
2017-09-07 16:55:33 -0700

    Fix vowel view pathnames

--- app/controllers/vowels_controller.rb
+++ app/controllers/vowels_controller.rb
@@ -1,23 +1,23 @@
 class VowelsController < ApplicationController
   def index
-  if params[:inventory]
-    @vowels = Vowel.find(params[:inventory])
-  else
-    @vowels = Vowel.all
+    if params[:inventory]
+      @vowels = Vowel.find(params[:inventory])
+    else
+      @vowels = Vowel.all
+    end
+    if params[:chartable]
+      render "/vowels/chartable_index"
+    else
+      render "/vowels/index"
+    end
   end
-  if params[:chartable]
-    render "api/vowels/chartable_index"
-  else
-    render "api/vowels/index"
-  end
-end
 
-def show
-  @vowel = Vowel.find(params[:id])
-  if @vowel
-    render "api/vowel/show"
-  else
-    render json: @vowel.errors.full_messages, status: 404
+  def show
+    @vowel = Vowel.find(params[:id])
+    if @vowel
+      render "/vowel/show"
+    else
+      render json: @vowel.errors.full_messages, status: 404
+    end
   end
-end
 end
